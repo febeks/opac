519c7b903e90c6e014ebc83793a52d7cca6c5445
febeks <frantisek.bekes@gmail.com>
2018-03-07 13:53:45 +0100

    fixed mobile size to full size

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Kniznica pre deti</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
